f26cd6ab17216b9f3bac7be1480494b6a2bf064f
KomalAyare <ayarekomal01@gmail.com>
2022-12-14 23:54:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <section className='page'>
+      <div className='page_content'>
+        <h1>COMING SOON...</h1>
+      </div>
+    </section>
+  );
+}
+
+export default App;
